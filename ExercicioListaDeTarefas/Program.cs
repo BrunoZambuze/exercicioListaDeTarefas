4e423b60795c2d867daaa03779d280c6f2d9afea
Bruno Zambuze <zambuzesilva@hotmail.com>
2023-11-06 11:33:55 -0300

    Lista de tarefas

---
+++ ExercicioListaDeTarefas/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExercicioListaDeTarefas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tarefas> Lista = new List<Tarefas>();
+            List<Tarefas> ListaRealizada = new List<Tarefas>();
+            int resp;
+            do
+            {
+                Console.WriteLine("\n\n***Lista de Tarefas***\n");
+                Console.WriteLine("[1]Adicionar Tarefa\n[2]Remover Tarefa\n[3]Editar Tarefa\n[4]Listar Tarefa\n[5]Sair");
+                resp = int.Parse(Console.ReadLine());
+                switch (resp)
+                {
+                    case 1:
+                        Console.WriteLine("---Descrição da Tarefa---");
+                        Console.Write("Nome:");
+                        string nome = Console.ReadLine();
+                        Console.Write("Descrição:");
+                        string descricao = Console.ReadLine();
+                        Console.Write("Data (dd/mm/aaaa):");
+                        DateTime data = DateTime.Parse(Console.ReadLine());
+                        Lista.Add(new Tarefas(nome, descricao, data));
+                        break;
+                    case 2:
+                        Console.Write("Digite a tarefa que deseja remover:");
+                        string remover = Console.ReadLine();
+                        Tarefas tarefa = Lista.Find(x => x.Nome == remover);
+                        if (tarefa != null)
+                        {
+                            Lista.Remove(tarefa);
+                        }
+                        else
+                        {
+                            Console.WriteLine("TAREFA NÃO ENCONTRADA");
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Qual tarefa você gostaria de editar o nome?:");
+                        string nomeAlt = Console.ReadLine();
+                        Tarefas altTarefa = Lista.Find(x => x.Nome == nomeAlt);
+                        if (altTarefa != null)
+                        {
+                            Console.Write("Digite o nome já alterado:");
+                            string nomeAlterado = Console.ReadLine();
+                            altTarefa.AlterarNome(nomeAlterado);
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Qual tarefa foi realizada?:");
+                        string tarefaRealizada = Console.ReadLine();
+                        Tarefas tarefaR = Lista.Find(x => x.Nome == tarefaRealizada);
+                        ListaRealizada = Lista.FindAll(x => x.Nome == tarefaR.Nome);
+                        Tarefas ListaR2 = ListaRealizada.Find(x => x.Status == "Pendente");
+                        if (ListaR2 != null)
+                        {
+                            ListaR2.AlterarStatus();
+                        }
+                        Lista.Remove(tarefaR);
+                        break;
+                }
+
+                Console.Clear();
+                Console.WriteLine("------TAREFAS------");
+                if (Lista.Count == 0)
+                {
+                    Console.WriteLine("\nLISTA VAZIA\n");
+                }
+                else
+                {
+                    foreach (Tarefas obj in Lista)
+                    {
+                        Console.WriteLine($"\n{obj}");
+                    }
+                }
+
+                Console.WriteLine("\n------TAREFAS REALIZADAS------");
+                if (ListaRealizada.Count == 0)
+                {
+                    Console.WriteLine("\nLISTA VAZIA\n");
+                }
+                else
+                {
+                    foreach (Tarefas obj in ListaRealizada)
+                    {
+                        Console.WriteLine($"{obj}");
+                    }
+                }
+            }
+            while (resp != 5);
+        }
+    }
+}
---
+++ ExercicioListaDeTarefas/Tarefas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExercicioListaDeTarefas
+{
+    internal class Tarefas
+    {
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public DateTime Data { get; private set; }
+        public string Status { get; private set; }
+
+        //Construtores
+        public Tarefas(string nome, string descricao, DateTime data)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Data = data;
+            Status = "Pendente";
+        }
+
+        //Função para editar nome da tarefa
+        public void AlterarNome(string altNome)
+        {
+            Nome = altNome;
+        }
+
+        //Função para alterar o status da tarefa
+        public void AlterarStatus()
+        {
+            Status = "✓ Feito";
+        }
+
+        //Exibir tarefas
+        public override string ToString()
+        {
+            return $"Nome:{Nome}\nDescrição:{Descricao}\nData de Vencimento:{Data}\nStatus:{Status}";
+        }
+    }
+}
